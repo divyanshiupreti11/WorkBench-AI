a0c1a51b882a8c7c5580670693003359be413b9f
divyanhiupreti11 <upretidivyanshi@gmail.com>
2026-07-11 16:00:57 +0530

    Add chat health route

--- backend/services/chat/index.js
+++ backend/services/chat/index.js
@@ -4,17 +4,23 @@ import router from "./routes/chat.routes.js";
 import connectDB from "./config/db.js";
 
 dotenv.config();
+
 const app = express();
 app.use(express.json());
-const port=process.env.PORT
 
+const port = process.env.PORT;
 
-app.use("/",router)
+// Health Check
+app.get("/", (req, res) => {
+  res.status(200).json({
+    service: "chat",
+    status: "ok",
+  });
+});
 
+app.use("/", router);
 
 app.listen(port, () => {
-    connectDB()
-  console.log(
-    `chat service running on ${port}`
-  );
+  connectDB();
+  console.log(`chat service running on ${port}`);
 });
